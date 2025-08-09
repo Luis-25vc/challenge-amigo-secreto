5e0d32ed44f99dc73033914eebde42be3926acf0
Luis-25vc <caps.kas30@gmail.com>
2025-08-09 03:23:14 -0400

    Arreglo de js primero

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 <h1>Juego del amigo secreto</h1>
 -Estado de proyecto:  En construccion.
+ingresar datos a la lista : -Logrado
+validacion de entrada     : -Logrado
+limpiar el input          : -Logrado
--- app.js
+++ app.js
@@ -1,1 +1,45 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo(){
+    let nombreDeAmigo = document.getElementById('amigo').value;
+    console.log(typeof(nombreDeAmigo));
+    //console.log(nombreDeAmigo);
+   
+    if (nombreDeAmigo == "" ){
+        alert('porfavor rellenar');
+    }else{
+        amigos.push(nombreDeAmigo);
+        console.log(amigos.length);
+        console.log(amigos);
+        document.getElementById('ruleta').removeAttribute('disabled');
+    }
+    let lista = document.querySelector('ul');
+    lista.innerHTML = (`${amigos}`);
+    limpiarCaja();
+}
+
+function limpiarCaja(){
+    document.querySelector('#amigo').value = '';
+}
+
+function sortearAmigo(){
+    
+}
+
+console.log(amigos)
+
+// function mostrarLista(amigos, listaAmigos){
+//     let lista = document.getElementById('listaAmigos')
+//     if (!lista) {
+//       console.error(`No se encontró el elemento con ID: ${listaAmigos}`);
+//       return;
+//     amigos.forEach(item => {
+//     const listItem = document.createElement("li");
+//     listItem.textContent = item;
+//     lista.appendChild(listItem);
+//   });
+// }
+// }
+// let lista = document.querySelector('ul')
+// lista.innerHTML = (`luis , ${amigos}`)
